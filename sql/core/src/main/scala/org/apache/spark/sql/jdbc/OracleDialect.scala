59f725f6df03343b7d5278366feaddefa0273de1
Enrico Minack <github@enrico.minack.dev>
2023-10-25 20:07:03 +0200

    Fix after rebase master

--- sql/core/src/main/scala/org/apache/spark/sql/jdbc/OracleDialect.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/jdbc/OracleDialect.scala
@@ -242,7 +242,7 @@ private case class OracleDialect() extends JdbcDialect with SQLConfHelper with M
   override def getMergeQuery(
       sourceTableName: String,
       destinationTableName: String,
-      columns: Array[String],
+      columns: Array[StructField],
       keyColumns: Array[String]): String = {
     // Oracle dialect does not like a few bits of the standard SQL MERGE command
     super.getMergeQuery(sourceTableName, destinationTableName, columns, keyColumns)
